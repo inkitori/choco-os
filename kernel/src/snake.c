da9e150e7e7c95e834df3788792f19e22f597952
enyoukai <an.joseph0001@gmail.com>
2026-03-14 06:47:08 -0700

    Fixing snake to randomize apple positions + adding self collisions

--- kernel/src/snake.c
+++ kernel/src/snake.c
@@ -13,20 +13,56 @@ static SnakeBody prev_snake[100];
 static Direction direction = DOWN;
 static uint64_t snake_size = 1;
 
-static uint64_t apple_x = 1;
-static uint64_t apple_y = 1;
+static uint64_t apple_x = 5;
+static uint64_t apple_y = 5;
 
 static bool just_ate_apple = false;
 static bool kill_sig = false;
 
+static uint32_t rng_seed = 12345;
+
 uint64_t tail_x;
 uint64_t tail_y;
 
+static uint32_t get_random()
+{
+	rng_seed = rng_seed * 1664525 + 1013904223;
+	return rng_seed;
+}
+
+static void snake_reset()
+{
+	direction = DOWN;
+	snake_size = 1;
+	kill_sig = false;
+	just_ate_apple = false;
+
+	snake[0].coordinates_x = 5;
+	snake[0].coordinates_y = 5;
+
+	uint8_t max_x = framebuffer_get_width() / GRID_SIZE;
+	uint8_t max_y = framebuffer_get_height() / GRID_SIZE;
+	if (max_x == 0) max_x = 1;
+	if (max_y == 0) max_y = 1;
+
+	apple_x = get_random() % max_x;
+	apple_y = get_random() % max_y;
+}
+
 static void snake_update()
 {
+	uint8_t max_x = framebuffer_get_width() / GRID_SIZE;
+	uint8_t max_y = framebuffer_get_height() / GRID_SIZE;
+	if (max_x == 0) max_x = 1;
+	if (max_y == 0) max_y = 1;
+
+	uint8_t current_tail_x = snake[snake_size - 1].coordinates_x;
+	uint8_t current_tail_y = snake[snake_size - 1].coordinates_y;
+
 	if (just_ate_apple)
 	{
-		snake_size++;
+		if (snake_size < 100)
+			snake_size++;
 		snake[snake_size - 1].coordinates_x = tail_x;
 		snake[snake_size - 1].coordinates_y = tail_y;
 
@@ -45,13 +81,13 @@ static void snake_update()
 		kill_sig = true;
 		return;
 	}
-	if (key == A)
+	if (key == A && direction != RIGHT)
 		direction = LEFT;
-	else if (key == D)
+	else if (key == D && direction != LEFT)
 		direction = RIGHT;
-	else if (key == W)
+	else if (key == W && direction != DOWN)
 		direction = UP;
-	else if (key == S)
+	else if (key == S && direction != UP)
 		direction = DOWN;
 
 	if (direction == LEFT)
@@ -63,15 +99,42 @@ static void snake_update()
 	else if (direction == DOWN)
 		snake[0].coordinates_y++;
 
+	// Wrap around
+	if (snake[0].coordinates_x >= max_x)
+	{
+		if (direction == LEFT)
+			snake[0].coordinates_x = max_x - 1;
+		else
+			snake[0].coordinates_x = 0;
+	}
+	if (snake[0].coordinates_y >= max_y)
+	{
+		if (direction == UP)
+			snake[0].coordinates_y = max_y - 1;
+		else
+			snake[0].coordinates_y = 0;
+	}
+
+	// Self-collision
+	for (uint64_t i = 1; i < snake_size; i++)
+	{
+		if (snake[0].coordinates_x == snake[i].coordinates_x &&
+			snake[0].coordinates_y == snake[i].coordinates_y)
+		{
+			snake_reset();
+			return;
+		}
+	}
+
 	if (snake[0].coordinates_x == apple_x && snake[0].coordinates_y == apple_y)
 	{
-		apple_x += 1;
-		apple_y += 1;
+		apple_x = get_random() % max_x;
+		apple_y = get_random() % max_y;
 
 		just_ate_apple = true;
 
-		tail_x = snake[snake_size - 1].coordinates_x;
-		tail_y = snake[snake_size - 1].coordinates_y;
+		tail_x = current_tail_x;
+		tail_y = current_tail_y;
 	}
 }
 
@@ -85,26 +148,18 @@ static void snake_render()
 	}
 
 	framebuffer_draw_rect(apple_x * GRID_SIZE + 5, apple_y * GRID_SIZE + 5, GRID_SIZE - 10, GRID_SIZE - 10, 0xFF0000);
-}
 
-static void snake_reset()
-{
-	direction = DOWN;
-	snake_size = 1;
-	kill_sig = false;
-	just_ate_apple = false;
-
-	apple_x = 1;
-	apple_y = 1;
+	framebuffer_swap();
 }
 
 void snake_init()
 {
+	rng_seed = timer_get_ticks();
+	if (rng_seed == 0) rng_seed = 12345;
+
 	snake_reset();
 
-	snake[0].coordinates_x = 0;
-	snake[0].coordinates_y = 0;
-
+	framebuffer_set_double_buffer(true);
 	framebuffer_clear(0x000000);
 
 	uint64_t start_time = timer_get_ticks();
@@ -112,7 +167,10 @@ void snake_init()
 	while (1)
 	{
 		if (kill_sig)
+		{
+			framebuffer_set_double_buffer(false);
 			return;
+		}
 		if (timer_get_ticks() - start_time >= 10)
 		{
 			start_time = timer_get_ticks();
